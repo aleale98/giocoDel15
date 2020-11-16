08741089726eb2ccd9c202f1e5461cc6de0f8b11
aleale98 <alessiomungelli@gmail.com>
2020-11-16 14:59:03 +0100

    Add files via upload

---
+++ fifteen.js
@@ -0,0 +1,79 @@
+
+function init(){
+    var tiles = Array.from(document.getElementById("puzzlearea").querySelectorAll("div"));
+    console.log(typeof tiles);
+    var ultimo = document.getElementById("puzzlearea").querySelector("#tile_4_4");
+    for(let i = 0; i < tiles.length; i++){
+            tiles[i].addEventListener("click", ev => cambia(tiles, tiles[i], ultimo));
+            tiles[i].addEventListener("mouseover", ev => cambiaColore(tiles, tiles[i], ultimo));
+            tiles[i].addEventListener("mouseout", ev => restore(tiles[i]));
+            tiles[i].style.order = i;
+            tiles[i].setAttribute("disabled", true);   
+    }
+    document.getElementById("shufflebutton").addEventListener("click", ev => gioca(tiles));
+   
+}
+
+
+function test(){
+    alert("button pressed");
+}
+
+function gioca(tiles){
+    console.log(typeof tiles);
+    //i = 0;
+    /*while (i<15) {
+        a = Math.random() * 15 >>> 0;
+        b = Math.random() * 15 >>> 0;
+        if (a==b) continue;
+        scambia(tiles[a], tiles[b]);
+        i++;
+    }*/
+};
+
+function scambia({style: a}, {style: b}) {
+	temp=a.order;
+	a.order=b.order;
+	b.order=temp;
+}
+
+function getX({style}) {
+    return parseInt(style.order)%4;
+}
+function getY({style}) {
+    return parseInt(style.order)/4 >>> 0;
+}
+
+function cambiaColore(tiles, tile, ultimo){
+    xWhite = getX(ultimo);
+    yWhite = getY(ultimo);
+    x = getX(tile);
+    y = getY(tile);
+    dx=Math.abs(xWhite-x);
+    dy=Math.abs(yWhite-y);
+    if((dx == 0 && dy == 1) || (dx == 1 && dy == 0)){
+        tile.setAttribute("disabled", false);
+    }
+}
+
+function restore(tile){
+    tile.setAttribute("disabled", true);
+}
+
+function cambia(tiles, tile, ultimo){
+    xWhite = getX(ultimo);
+    yWhite = getY(ultimo);
+    x = getX(tile);
+    y = getY(tile);
+    dx=Math.abs(xWhite-x);
+    dy=Math.abs(yWhite-y);
+    if((dx == 0 && dy == 1) || (dx == 1 && dy == 0)){
+        scambia(ultimo, tile);
+        if (tiles.every((p, i) => i == getX(p) + getY(p) * 4)) {
+            alert("Hai vinto!");
+        }
+    }  
+}
+
+
+window.onload = init;
